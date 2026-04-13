0712e73a7882ea4a6b32bc88e446532aa07f8e0e
Hiennguyen34 <hiennguyen6a6@gmail.com>
2026-04-13 21:11:57 +0700

    update dashboard of lecturer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
 import './App.css';
-import MainLayout from './layouts/MainLayout.jsx';
+// import MainLayout from './layouts/MainLayout.jsx';
+import Dashboard from './lecturer/dashboard.jsx';
 
 function App() {
   return (
-    <MainLayout userName="Nguyễn Thị Diệu Hiền">
-      <div style={{ width: '100%', minHeight: '80vh', padding: '0' }}>
-        {/*<h2>Chào mừng bạn đến với ứng dụng của chúng tôi</h2>
-        <p>Đây là một đoạn văn mẫu.</p>*/}
-      </div>
-    </MainLayout>
+    // <MainLayout userName="Nguyễn Thị Diệu Hiền">
+    //   <div style={{ width: '100%', minHeight: '80vh', padding: '0' }}>
+    //     {/*<h2>Chào mừng bạn đến với ứng dụng của chúng tôi</h2>
+    //     <p>Đây là một đoạn văn mẫu.</p>*/}
+    //   </div>
+    // </MainLayout>
+    <Dashboard/>
   );
 }
 
